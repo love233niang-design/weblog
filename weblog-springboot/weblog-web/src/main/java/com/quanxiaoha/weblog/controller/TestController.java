9153028e416f547f85ecf130baf68e3df5c52768
hq <love233@139.com>
2026-06-15 11:22:22 +0800

    自定义响应工具类

--- weblog-springboot/weblog-web/src/main/java/com/quanxiaoha/weblog/controller/TestController.java
+++ weblog-springboot/weblog-web/src/main/java/com/quanxiaoha/weblog/controller/TestController.java
@@ -1,9 +1,9 @@
 package com.quanxiaoha.weblog.controller;
 
 import com.quanxiaoha.weblog.common.aspect.ApiOperationLog;
+import com.quanxiaoha.weblog.common.utils.Response;
 import com.quanxiaoha.weblog.model.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
@@ -19,24 +19,36 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-
-    public ResponseEntity<String> test(@RequestBody
-                                       @Validated // 对user 对象 user 对象进行 JSR-303 数据校验
-                                       User user,
-                                       BindingResult bindingResult // BindingResult 验证的结果对象，其中包含了错误信息
-    ) {
-        // 是否存在校验错误
+//
+//    public ResponseEntity<String> test(@RequestBody
+//                                       @Validated // 对user 对象 user 对象进行 JSR-303 数据校验
+//                                       User user,
+//                                       BindingResult bindingResult // BindingResult 验证的结果对象，其中包含了错误信息
+//    ) {
+//        // 是否存在校验错误
+//        if (bindingResult.hasErrors()) {
+//            // 获取校验不通过字段的提示信息
+//            String errorMsg = bindingResult.getFieldErrors()
+//                    .stream()
+//                    .map(FieldError::getDefaultMessage)
+//                    .collect(Collectors.joining(", "));
+//
+//            return ResponseEntity.badRequest().body(errorMsg);
+//        }
+//
+//        // 返参
+//        return ResponseEntity.ok("参数没有任何问题");
+//    }
+    public Response test(@RequestBody @Validated User user, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            // 获取校验不通过字段的提示信息
             String errorMsg = bindingResult.getFieldErrors()
                     .stream()
                     .map(FieldError::getDefaultMessage)
                     .collect(Collectors.joining(", "));
-
-            return ResponseEntity.badRequest().body(errorMsg);
+            return Response.fail(errorMsg);
         }
 
-        // 返参
-        return ResponseEntity.ok("参数没有任何问题");
+        // 返回参
+        return Response.success();
     }
 }
