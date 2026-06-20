deae884d5b125cefeb020ff545af08c17c402e69
hq <love233@139.com>
2026-06-20 10:01:18 +0800

    修改密码功能实现

--- weblog-springboot/weblog-module-common/src/main/java/com/quanxiaoha/weblog/common/domain/mapper/UserMapper.java
+++ weblog-springboot/weblog-module-common/src/main/java/com/quanxiaoha/weblog/common/domain/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.quanxiaoha.weblog.common.domain.mapper;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.quanxiaoha.weblog.common.domain.dos.UserDO;
 
@@ -9,5 +10,17 @@ public interface UserMapper extends BaseMapper<UserDO> {
         LambdaQueryWrapper<UserDO> wrapper = new LambdaQueryWrapper<>();
         wrapper.eq(UserDO::getUsername, username);
         return selectOne(wrapper);
+    }
+
+    default int updatePasswordByUsername(String username, String password) {
+        LambdaUpdateWrapper<UserDO> wrapper = new LambdaUpdateWrapper<>();
+        // 设置要更新的字段
+        wrapper.set(UserDO::getPassword, password);
+        wrapper.eq(UserDO::getUsername, username);
+        // 更新条件
+        wrapper.eq(UserDO::getUsername, username);
+
+        return update(null, wrapper);
+
     }
 }
