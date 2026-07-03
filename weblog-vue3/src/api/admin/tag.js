c3e0853b10bf2848343a8a444a9847753013b0c9
hq <love233niang@gmail.com>
2026-07-03 21:18:03 +0800

    前端文章管理界面

--- weblog-vue3/src/api/admin/tag.js
+++ weblog-vue3/src/api/admin/tag.js
@@ -14,3 +14,7 @@ export function addTag(data) {
 export function deleteTag(id) {
     return axios.post("/admin/tag/delete", {id})
 }
+// 根据标签名模糊查询
+export function searchTags(key) {
+    return axios.post("/admin/tag/search", {key})
+}
