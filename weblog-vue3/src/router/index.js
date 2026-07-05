1732b790dc2c71717b1436c3ba41e40686be417c
hq <love233niang@gmail.com>
2026-07-05 15:45:09 +0800

    前端归档及分类界面开发

--- weblog-vue3/src/router/index.js
+++ weblog-vue3/src/router/index.js
@@ -3,13 +3,28 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from '@/pages/admin/login.vue'
 import AdminIndex from '@/pages/admin/index.vue'
 import Admin from '@/layouts/admin/admin.vue'
-
 import AdminArticleList from '@/pages/admin/article-list.vue'
 import AdminCategoryList from '@/pages/admin/category-list.vue'
 import AdminTagList from '@/pages/admin/tag-list.vue'
 import AdminBlogSetting from '@/pages/admin/blog-setting.vue'
+import ArchiveList from '@/pages/frontend/archive-list.vue'
+import CategoryList from '@/pages/frontend/category-list.vue'
 // 统一在这里声明所有路由
 const routes = [
+    {
+        path: '/category/list', // 分类页
+        component: CategoryList,
+        meta: { // meta 信息
+            title: 'Weblog 分类页'
+        }
+    },
+    {
+        path: '/archive/list', // 归档页
+        component: ArchiveList,
+        meta: { // meta 信息
+            title: 'Weblog 归档页'
+        }
+    },
     {
         path: "/admin", // 后台首页
         component: Admin,
