4571260cb7b15cb13ee6424535b64b9915290422
hq <love233niang@gmail.com>
2026-07-07 11:24:54 +0800

    后端集成 Markdown 工具

---
+++ weblog-springboot/weblog-web/src/main/java/com/love233niang/weblog/markdown/MarkdownHelper.java
@@ -0,0 +1,64 @@
+package com.love233niang.weblog.markdown;
+
+import com.love233niang.weblog.markdown.provider.NofollowLinkAttributeProvider;
+import com.love233niang.weblog.markdown.renderer.ImageNodeRenderer;
+import org.commonmark.Extension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
+import org.commonmark.ext.heading.anchor.HeadingAnchorExtension;
+import org.commonmark.ext.image.attributes.ImageAttributesExtension;
+import org.commonmark.ext.task.list.items.TaskListItemsExtension;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MarkdownHelper {
+
+    /**
+     * Markdown 解析器
+     */
+    private final static Parser PARSER;
+
+    /**
+     * HTML 渲染器
+     */
+    private final static HtmlRenderer HTML_RENDERER;
+
+    static {
+        List<Extension> extensions = Arrays.asList(
+                TablesExtension.create(), // 添加表格支持
+                HeadingAnchorExtension.create(), // 添加标题锚点支持
+                ImageAttributesExtension.create(), // 添加图片属性支持
+                TaskListItemsExtension.create() // 添加任务列表项支持
+        );
+
+        PARSER = Parser.builder().extensions(extensions).build();
+        HTML_RENDERER = HtmlRenderer
+                .builder()
+                .extensions(extensions)
+                .attributeProviderFactory(context -> new NofollowLinkAttributeProvider())
+                .nodeRendererFactory(context -> new ImageNodeRenderer(context))
+                .build();
+    }
+
+    /**
+     * 将 Markdown 转换为 HTML
+     *
+     * @param markdown
+     * @return
+     */
+    public static String convertMarkdown2Html(String markdown) {
+        Node document = PARSER.parse(markdown);
+        return HTML_RENDERER.render(document);
+    }
+
+    public static void main(String[] args) {
+        String markdown = "![图 1-1 技术栈](https://img.quanxiaoha.com/quanxiaoha/169560181378937 \"图 1-1 技术栈\"){width=100 height=100}";
+        System.out.println(MarkdownHelper.convertMarkdown2Html(markdown));
+
+    }
+
+
+}
